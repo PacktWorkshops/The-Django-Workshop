fc8a1252d9a0b2169c915bbfeb27db837880bb6a
Shivani Mehta <sssr.mehta@gmail.com>
2019-11-11 14:32:38 +1100

    Revert "Code for Chapter 3"
    This reverts commit e83299d316a886f3e68513fb066c8e45a011f490.

--- Exercise 3.5/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from .models import Book
-
-
-def detail(request, book_title):
-    book_detail = Book.objects.filter(title=book_title).values_list("publication_date", "isbn", "publisher")
-
-    return JsonResponse({'results': list(book_detail)})
