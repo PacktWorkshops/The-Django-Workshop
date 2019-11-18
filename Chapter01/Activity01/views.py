c08d4c23d4e1685b99c46e9cbbc11b5eb351e44a
Ben Shaw <ben@beneboy.co.nz>
2019-11-18 14:06:02 +1300

    Added refactored chapter 1 exercises

--- Chapter01/Activity01/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    name = request.GET.get("name") or "world"
-    return render(request, "base.html", {"name": name})
--- Chapter01/Activity02/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-import reviews.views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', reviews.views.index),
-    path('goodbye', reviews.views.goodbye)
-]
