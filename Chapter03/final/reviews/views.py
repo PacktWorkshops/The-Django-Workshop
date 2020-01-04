5ebf1ed143fd44e8f104196905abb5e624d8ff79
Bharath Chandra <bharathchandraks@gmail.com>
2020-01-05 06:15:51 +1100

    End of function based views

--- Chapter03/final/reviews/views.py
+++ Chapter03/final/reviews/views.py
@@ -1,24 +1,42 @@
 from django.shortcuts import render
 
-from .models import Book
-
-from django.http import HttpResponse
-import datetime
-
-from django import views
+from .models import Book, Review
+from .utils import average_rating
 
 
 def books_list(request):
     books = Book.objects.all()
+    book_list = []
+    for book in books:
+        reviews = book.review_set.all()
+        if reviews:
+            book_rating = average_rating([review.rating for review in reviews])
+            number_of_reviews = len(reviews)
+        else:
+            book_rating = None
+            number_of_reviews = 0
+        book_list.append({'book': book, 'book_rating': book_rating, 'number_of_reviews': number_of_reviews})
+
     context = {
-        'books': books
+        'book_list': book_list
     }
     return render(request, 'reviews/books_list.html', context)
 
 
 def book_detail(request, id):
     book = Book.objects.get(id=id)
-    context = {
-        'book': book
-    }
+    reviews = book.review_set.all()
+    if reviews:
+        book_rating = average_rating([review.rating for review in reviews])
+        context = {
+            'book': book,
+            'book_rating': book_rating,
+            'reviews': reviews
+        }
+    else:
+        context = {
+            'book': book,
+            'book_rating': None,
+            'reviews': None
+        }
     return render(request, 'reviews/books_detail.html', context)
