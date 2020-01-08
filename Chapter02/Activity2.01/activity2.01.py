20b010383e35033a18b8aa949d539649adffa344
Bharath Chandra <bharathchandraks@gmail.com>
2020-01-09 07:01:41 +1100

    Added activity to chapter02

--- Chapter02/Activity2.01/activity2.01.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-from reviews.models import Contributor
-
-contributor = Contributor.objects.filter(book__title='The Tailsman')
-print(contributor)
--- Chapter02/Activity2.02/activity2.02.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-from reviews.models import Book
-
-book = Book.objects.get(title='The Tailsman')
-contributors = book.contributors.all()
-print(contributors)
