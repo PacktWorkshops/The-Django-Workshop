5b82407c8251710f4c884ed8104cc0a4d49d2f13
Ben Shaw <ben@beneboy.co.nz>
2020-01-15 06:34:39 +1300

    Update Chapter 3 Final code to incorporate chapter 1's final code

--- Chapter03/final/bookr/reviews/urls.py
+++ Chapter03/final/bookr/reviews/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.books_list),
-    path('book/<int:id>/', views.book_detail)
+    path('books/', views.book_list, name='book_list'),
+    path('books/<int:pk>/', views.book_detail, name='book_detail')
 ]
--- Chapter03/final/bookr/reviews/views.py
+++ Chapter03/final/bookr/reviews/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Book
 from .utils import average_rating
 
 
-def books_list(request):
+def index(request):
+    return render(request, "base.html")
+
+
+def book_search(request):
+    search_text = request.GET.get("search", "")
+    return render(request, "reviews/search-results.html", {"search_text": search_text})
+
+
+def book_list(request):
     books = Book.objects.all()
-    book_list = []
+    books_with_reviews = []
     for book in books:
         reviews = book.review_set.all()
         if reviews:
@@ -15,28 +24,28 @@ def books_list(request):
         else:
             book_rating = None
             number_of_reviews = 0
-        book_list.append({'book': book, 'book_rating': book_rating, 'number_of_reviews': number_of_reviews})
+        books_with_reviews.append({"book": book, "book_rating": book_rating, "number_of_reviews": number_of_reviews})
 
     context = {
-        'book_list': book_list
+        "book_list": books_with_reviews
     }
-    return render(request, 'reviews/books_list.html', context)
+    return render(request, "reviews/book_list.html", context)
 
 
-def book_detail(request, id):
-    book = Book.objects.get(id=id)
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
     reviews = book.review_set.all()
     if reviews:
         book_rating = average_rating([review.rating for review in reviews])
         context = {
-            'book': book,
-            'book_rating': book_rating,
-            'reviews': reviews
+            "book": book,
+            "book_rating": book_rating,
+            "reviews": reviews
         }
     else:
         context = {
-            'book': book,
-            'book_rating': None,
-            'reviews': None
+            "book": book,
+            "book_rating": None,
+            "reviews": None
         }
-    return render(request, 'reviews/books_detail.html', context)
+    return render(request, "reviews/book_detail.html", context)
