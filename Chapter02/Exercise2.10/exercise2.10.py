64f689f5a6827843f7ac449d5dd72466f4aa052c
Bharath Chandra <bharathchandraks@gmail.com>
2020-01-31 21:49:20 +1100

    Exercise 2.10 combined

--- Chapter02/Exercise2.10/exercise2.10.py
+++ Chapter02/Exercise2.10/exercise2.10.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 
-from reviews.models import Contributor
+from reviews.models import Contributor, Publisher
 
 Contributor.objects.filter(book__title='The Talisman')
+
+Publisher.objects.get(book__title='Advanced Deep Learning with Keras')
+
+book = Book.objects.get(title='The Talisman')
+
+print(book.publisher)
+
+publisher = Publisher.objects.get(name='Pocket Books')
+
+publisher.book_set.all()
