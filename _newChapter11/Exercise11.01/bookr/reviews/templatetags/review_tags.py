955d71b7d09906276a0ce75f62f1dd5aca2bf811
Bharath Chandra <bharathchandraks@gmail.com>
2020-05-26 00:27:35 +1000

    Minor correction in review tags

--- _newChapter11/Exercise11.01/bookr/reviews/templatetags/review_tags.py
+++ _newChapter11/Exercise11.01/bookr/reviews/templatetags/review_tags.py
@@ -1,9 +1,11 @@
 from django import template
 
-from ..models import Book
+from reviews.models import Book
+
+
 register = template.Library()
 
 
 @register.simple_tag(name='books_count')
 def number_of_books():
-    return Book.objects.all().count()
+    return Book.objects.count()
--- _newChapter11/Exercise11.04/bookr/reviews/templatetags/review_tags.py
+++ _newChapter11/Exercise11.04/bookr/reviews/templatetags/review_tags.py
@@ -1,12 +1,14 @@
 from django import template
 
-from ..models import Book
+from reviews.models import Book
+
+
 register = template.Library()
 
 
 @register.simple_tag(name='books_count')
 def number_of_books():
-    return Book.objects.all().count()
+    return Book.objects.count()
 
 
 @register.inclusion_tag('reviews/recent_books.html')
