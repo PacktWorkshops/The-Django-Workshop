d51d7553f50076a7dc3e96aba4d45f7141a659c5
Timothy Miller <tmiller@unimelb.edu.au>
2020-08-08 18:12:41 +1000

    Chapter 4 Subclassing AdminSite

--- Chapter04/CustomisingTheModelClass/bookr/reviews/admin.py
+++ Chapter04/CustomisingTheModelClass/bookr/reviews/admin.py
@@ -1,11 +1,18 @@
-from django.contrib import admin
+from django.contrib.admin import AdminSite
+from reviews.models import (Publisher, Contributor, Book,
+        BookContributor, Review)
 
-from reviews.models import Publisher, Contributor, Book, BookContributor, Review
+class BookrAdminSite(AdminSite):
+    title_header = 'Bookr Admin'
+    site_header = 'Bookr administration'
+    index_title = 'Bookr site admin'
+
+admin_site = BookrAdminSite(name='bookr')
 
 # Register your models here.
-admin.site.register(Publisher)
-admin.site.register(Contributor)
-admin.site.register(Book)
-admin.site.register(BookContributor)
-admin.site.register(Review)
+admin_site.register(Publisher)
+admin_site.register(Contributor)
+admin_site.register(Book)
+admin_site.register(BookContributor)
+admin_site.register(Review)
 
