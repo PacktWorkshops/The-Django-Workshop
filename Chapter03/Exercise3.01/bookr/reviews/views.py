d1e78950e0fd2bfc2f801ecea87916fe0759d518
Andrew Bird <andrew.bird@vesparum.com>
2020-09-01 10:26:06 +1000

    Chapter 3 improvements

--- Chapter03/Exercise3.01/bookr/reviews/views.py
+++ Chapter03/Exercise3.01/bookr/reviews/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
+from .models import Book
 
 
 def welcome_view(request):
-    message = "<html><h1>Welcome to Bookr!</h1></html>"
+    message = f"<html><h1>Welcome to Bookr!</h1> <p>{Book.objects.count()} books and counting!</p></html>"
     return HttpResponse(message)
