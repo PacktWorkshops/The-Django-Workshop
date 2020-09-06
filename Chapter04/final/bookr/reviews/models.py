b0511586ae3f88df741fd68177d2b8aa91418e76
Chris Guest <chris.guest@managedfunctions.com>
2020-09-07 00:37:44 +1000

    Update Chapter 4 with Chapter 3 changes

--- Chapter04/final/bookr/reviews/models.py
+++ Chapter04/final/bookr/reviews/models.py
@@ -66,6 +66,9 @@ class BookContributor(models.Model):
     role = models.CharField(verbose_name="The role this contributor had in the book.",
                             choices=ContributionRole.choices, max_length=20)
 
+    def __str__(self):
+        return "{} {} {}".format(self.contributor.initialled_name(), self.role, self.book.isbn)
+    
 
 class Review(models.Model):
     content = models.TextField(help_text="The Review text.")
@@ -73,8 +76,10 @@ class Review(models.Model):
     date_created = models.DateTimeField(auto_now_add=True,
                                         help_text="The date and time the review was created.")
     date_edited = models.DateTimeField(null=True,
-                                       help_text='''The date and time the review was last edited.'''
-                                       )
+                                       help_text="The date and time the review was last edited.")
     creator = models.ForeignKey(auth.get_user_model(), on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE,
                              help_text="The Book that this review is for.")
+
+    def __str__(self):
+        return "{} - {}".format(self.creator.username, self.book.title)
