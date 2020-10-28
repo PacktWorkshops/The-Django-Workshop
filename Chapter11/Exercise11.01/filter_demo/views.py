69df47d01dec46981d4813e13db55de2cfd396d9
Saurabh Badhwar <sbadhwar@sbadhwar-mn1.linkedin.biz>
2020-10-28 09:29:27 +0530

    Fix typos in file naming and variable declaration for templates

--- Chapter11/Exercise11.01/filter_demo/views.py
+++ Chapter11/Exercise11.01/filter_demo/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 def index(request):
     names = "john,doe,mark,swain"
-    return render(request, "index.html", {'name': names})
+    return render(request, "index.html", {'names': names})
