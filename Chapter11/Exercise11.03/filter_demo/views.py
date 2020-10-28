fc6230065db588fa8470d380bb1993ac7cf43486
Saurabh Badhwar <sbadhwar@sbadhwar-mn1.linkedin.biz>
2020-10-28 18:04:01 +0530

    Fix a typo in the message

--- Chapter11/Exercise11.03/filter_demo/views.py
+++ Chapter11/Exercise11.03/filter_demo/views.py
@@ -7,7 +7,7 @@ def index(request):
 
 def greeting_view(request):
     books = {
-        "The Night Rider": "Ben Author",
+        "The night rider": "Ben Author",
         "The Justice": "Don Abeman"
     }
     return render(request, 'simple_tag_template.html', {'username': 'jdoe', 'books': books})
