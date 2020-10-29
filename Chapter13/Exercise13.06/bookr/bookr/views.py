cfa8c88aee010c7b80350bcdeb7e208bafc2627f
Saurabh Badhwar <sbadhwar@sbadhwar-mn1.linkedin.biz>
2020-10-29 21:57:12 +0530

    Sync changes in Exercise 13.06 to upstream repo

--- Chapter13/Exercise13.06/bookr/bookr/views.py
+++ Chapter13/Exercise13.06/bookr/bookr/views.py
@@ -36,39 +36,3 @@ def profile(request):
     return render(request, 'profile.html',
             {'user': user, 'books_read_plot': plot_html})
 
-
-@login_required
-def reading_history(request):
-    user = request.user.username
-    books_read = get_books_read(user)
-
-    # Create an object to create files in memory
-    temp_file = BytesIO()
-
-    # Start a new workbook
-    workbook = xlsxwriter.Workbook(temp_file)
-    worksheet = workbook.add_worksheet()
-    
-    # Prepare the data to be written
-    data = []
-    for book_read in books_read:
-        data.append([book_read['title'], str(book_read['completed_on'])])
-
-    # Write data to worksheet
-    for row in range(len(data)):
-        for col in range(len(data[row])):
-            worksheet.write(row, col, data[row][col])
-
-    # Close the workbook
-    workbook.close()
-
-    # Capture data from memory file
-    data_to_download = temp_file.getvalue()
-
-    # Prepare response for download
-    response = HttpResponse(content_type='application/vnd.ms-excel')
-    response['Content-Disposition'] = 'attachment; filename=reading_history.xlsx'
-    response.write(data_to_download)
-
-    return response
-
