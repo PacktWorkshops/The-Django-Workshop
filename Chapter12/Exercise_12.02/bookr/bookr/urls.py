41b7a65ffc2be7029212893865c32dbfabb2d2d5
Andrew Bird <andrew.bird@vesparum.com>
2020-12-03 17:04:45 +1100

    Add missing import

--- Chapter12/Exercise_12.02/bookr/bookr/urls.py
+++ Chapter12/Exercise_12.02/bookr/bookr/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
+import reviews.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
