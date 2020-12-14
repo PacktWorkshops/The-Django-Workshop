7af25b2d507c42c19f5e3de1315c525cb92cfd50
Saurabh Badhwar <sbadhwar@sbadhwar-mn1.linkedin.biz>
2020-12-14 11:39:13 +0530

    Sync Ch12 and Ch13

---
+++ Chapter13/final/bookr/reviews/api_views.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import authenticate
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.status import HTTP_404_NOT_FOUND, HTTP_200_OK
+from rest_framework.views import APIView
+
+from .models import Book
+from .serializers import BookSerializer
+
+
+class Login(APIView):
+
+    def post(self, request):
+        user = authenticate(username=request.data.get("username"), password=request.data.get("password"))
+        if not user:
+            return Response({'error': 'Credentials are incorrect or user does not exist'}, status=HTTP_404_NOT_FOUND)
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key}, status=HTTP_200_OK)
+
+
+class BookViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
