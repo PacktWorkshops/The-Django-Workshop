53cdd60b8761c9cbaec486799c6e11049cbbb05f
Saurabh Badhwar <sbadhwar@sbadhwar-mn3.linkedin.biz>
2020-12-17 20:00:24 +0530

    Fix Activity 13.01 by synchronizing code from previous exercise

--- Chapter13/Activity13.01/bookr/bookr/views.py
+++ Chapter13/Activity13.01/bookr/bookr/views.py
@@ -14,13 +14,14 @@ from .utils import get_books_read, get_books_read_by_month
 @login_required
 def profile(request):
     user = request.user
+    permissions = user.get_all_permissions()
     # Get the books read in different months this year
     books_read_by_month = get_books_read_by_month(user.username)
 
     # Initialize the Axis for graphs, X-Axis is months, Y-axis is books read
     months = [i+1 for i in range(12)]
     books_read = [0 for _ in range(12)]
-    
+
     # Set the value for books read per month on Y-Axis
     for num_books_read in books_read_by_month:
         list_index = num_books_read['date_created__month'] - 1
@@ -30,11 +31,11 @@ def profile(request):
     figure = graphs.Figure()
     scatter = graphs.Scatter(x=months, y=books_read)
     figure.add_trace(scatter)
+    figure.update_layout(xaxis_title="Month", yaxis_title="No. of books read")
     plot_html = plot(figure, output_type='div')
 
-    # Add to template
     return render(request, 'profile.html',
-            {'user': user, 'books_read_plot': plot_html})
+            {'user': user, 'permissions': permissions, 'books_read_plot': plot_html}
 
 
 @login_required
